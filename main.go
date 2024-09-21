8796efe8d91147af9772bd8340923b1c2636fc39
salmanfaris22 <salman567633800@gmail.com>
2024-09-21 19:28:46 +0530

    "server done"

--- main.go
+++ main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.come/salmanfaris22/Skill-map/controller"
-	"github.come/salmanfaris22/Skill-map/service"
-)
-
-var (
-	videoService    service.VideoService       = service.New()
-	videoController controller.VideoController = controller.New(videoService) // Fixed naming
+	"github.come/salmanfaris22/Skill-map/handler"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
-
-	server.POST("/videos", func(ctx *gin.Context) {
-		videoController.Save(ctx) // The Save method handles the response
-	})
-
+	server.POST("/login", handler.Logginehanler)
 	server.Run()
 }
